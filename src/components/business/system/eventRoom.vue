129faa3d48313f351dfa9c2acc70eca692cbe05f
‘cuichangxin’ <cuichangxin3@163.com>
2023-07-11 18:12:44 +0800

    iframe加载慢

--- src/components/business/system/eventRoom.vue
+++ src/components/business/system/eventRoom.vue
@@ -4,7 +4,8 @@
     v-loading="loading"
     element-loading-text="加载中..."
   >
-    <iframe id="iframe" class="iframe" src="http://172.20.10.2:3000/#/duty" frameborder="0"></iframe>
+    <!-- <iframe id="iframe" class="iframe" src="http://172.20.10.2:3000/#/duty" frameborder="0"></iframe> -->
+    <iframe id="iframe" class="iframe" src="http://192.168.30.117:3000/#/duty" frameborder="0"></iframe>
   </div>
 </template>
 <script setup>
