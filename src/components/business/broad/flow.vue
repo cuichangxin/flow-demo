6bc96123c064383dd0c7327913dc0fab18275313
cuichangxin <cuichangxin3@163.com>
2023-11-08 10:39:26 +0800

    修改地址

--- src/components/business/broad/flow.vue
+++ src/components/business/broad/flow.vue
@@ -1423,7 +1423,7 @@ function attrsNode() {
   }
 }
 function getFlow(json, o) {
-  axios.get(`http://192.168.242.94:8080/mock/flow/${json}.json`).then((res) => {
+  axios.get(`/mock/flow/${json}.json`).then((res) => {
     graphData.value = res
     console.log('1')
     init(o)
